84a8db8e493112760667b822468769bb59ff2b7a
Yasin <yasinkucuk_11@hotmail.com>
2021-01-16 15:06:04 +0300

    son düzenleme

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Uyg01
+# YKSoru-Cevap-Portali
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 9.1.7.
 
--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('uyg01 app is running!');
+    expect(page.getTitleText()).toEqual('YKSoru-Cevap-Portali app is running!');
   });
 
   afterEach(async () => {
